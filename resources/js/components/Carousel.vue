0be7c493138d08b833484e2eddd961624d4f5c3f
Prabhakar Mudliyar <mudliyarp@HCL.COM>
2018-12-28 19:55:26 +0530

    Carousel added to the about page

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="main-carousel">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import Flickity from 'flickity';
+    import "flickity/dist/flickity.min.css";
+    export default {
+        props: {
+            autoplay: { default: false},
+            wraparound: {default: true}
+        },
+        mounted() {
+            var flkty = new Flickity( '.main-carousel', {
+              "cellAlign": "left",
+              "contain": true,
+              "autoPlay": this.autoplay,
+              "wrapAround": this.wraparound
+
+            });
+        }
+    }
+</script>
+
--- resources/js/views/About.vue
+++ resources/js/views/About.vue
@@ -1,23 +1,34 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">The About Page</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
+    <div class="columns">
+        <div class="column">
+            <article class="message">
+                <div class="message-header">
+                    The Carousel Component With VUEJs
                 </div>
-            </div>
-        </div>
+                <div class="message-body">
+                    <carousel :autoplay="true">
+                        <img src="http://placeimg.com/640/480/any">
+                        <img src="http://placeimg.com/640/480/any?2">
+                        <img src="http://placeimg.com/640/480/any?3">
+                        <img src="http://placeimg.com/640/480/any?4">
+                        <img src="http://placeimg.com/640/480/any?5">
+                    </carousel>
+                    <amplify-confirm-sign-in v-bind:confirmSignInConfig="confirmSignInConfig"></amplify-confirm-sign-in>
+
+                </div>
+            </article>
+        </div>       
     </div>
+
 </template>
 
 <script>
+    import Carousel from '../Components/Carousel.vue';
     export default {
+        components: { Carousel },
         mounted() {
             console.log('Component mounted.')
         }
     }
 </script>
+
